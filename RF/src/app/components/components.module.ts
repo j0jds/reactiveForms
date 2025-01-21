36581d62e8ea0421822d70ec3145552e511aca5d
j0jds <mrlucassilva7342@gmail.com>
2025-01-21 15:43:13 -0300

    Create modules

---
+++ RF/src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { PipesModule } from "../pipes/pipes.module";
+import { AngularMaterialModule } from "../angular-material/angular-material.module";
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        AngularMaterialModule,
+        PipesModule
+    ],
+    exports: [
+
+    ],
+})
+export class ComponentsModule {
+
+}
