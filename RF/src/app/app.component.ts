588f93cde184f83471125af3cb0a28b329b3eb09
j0jds <mrlucassilva7342@gmail.com>
2025-01-22 16:45:21 -0300

    Create cities service

--- RF/src/app/app.component.ts
+++ RF/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { StatesService } from './services/states.service';
 import { CountriesService } from './services/countries.service';
 import { Component, OnInit } from '@angular/core';
+import { CitiesService } from './services/cities.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent implements OnInit {
   
   constructor(
     private readonly _countriesService: CountriesService,
-    private readonly _statesService: StatesService
+    private readonly _statesService: StatesService,
+    private readonly _citiesService: CitiesService
   ) {}
 
   ngOnInit(): void {
@@ -22,7 +24,11 @@ export class AppComponent implements OnInit {
 
     this._statesService.getStates('Brazil').subscribe((statesResponse) => {
       console.log('statesResponse', statesResponse)
-    }) 
+    });
+
+    this._citiesService.getCities('Brazil', 'Pernambuco').subscribe((citiesResponse) => {
+      console.log('citiesResponse', citiesResponse)
+    });
   }
 
 }
---
+++ RF/src/app/services/cities.service.ts
@@ -0,0 +1,28 @@
+import { map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class CitiesService {
+    
+    constructor(
+    private readonly _httpClient: HttpClient
+    ) {}
+
+  getCities(countryName: string, stateName: string) {
+    return this._httpClient.post<any>(
+      'https://countriesnow.space/api/v0.1/countries/state/cities',
+
+      {
+        country: countryName,
+        state: stateName,
+      }).pipe(
+        map((citiesResponse) => {
+            return citiesResponse.data;
+        })
+    );
+  }
+}
