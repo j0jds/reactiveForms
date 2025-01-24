d809ad9a735af71c3eb1fcfe8f8aaf440d2656c6
j0jds <mrlucassilva7342@gmail.com>
2025-01-24 14:28:29 -0300

    Added users service in OnInit

--- RF/src/app/app.component.ts
+++ RF/src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { UsersService } from './services/users.service';
 import { StatesService } from './services/states.service';
+import { CitiesService } from './services/cities.service';
 import { CountriesService } from './services/countries.service';
 import { Component, OnInit } from '@angular/core';
-import { CitiesService } from './services/cities.service';
 
 @Component({
   selector: 'app-root',
@@ -14,21 +15,25 @@ export class AppComponent implements OnInit {
   constructor(
     private readonly _countriesService: CountriesService,
     private readonly _statesService: StatesService,
-    private readonly _citiesService: CitiesService
+    private readonly _citiesService: CitiesService,
+    private readonly _usersService: UsersService,
   ) {}
 
   ngOnInit(): void {
     this._countriesService.getCountries().subscribe((countriesResponse) => {
-      console.log('countriesResponse', countriesResponse)
+      console.log('countriesResponse', countriesResponse);
     });
 
     this._statesService.getStates('Brazil').subscribe((statesResponse) => {
-      console.log('statesResponse', statesResponse)
+      console.log('statesResponse', statesResponse);
     });
 
     this._citiesService.getCities('Brazil', 'Pernambuco').subscribe((citiesResponse) => {
-      console.log('citiesResponse', citiesResponse)
+      console.log('citiesResponse', citiesResponse);
+    });
+    
+    this._usersService.getUsers().subscribe((usersListResponse) => {
+      console.log('usersListResponse', usersListResponse)
     });
   }
-
 }
