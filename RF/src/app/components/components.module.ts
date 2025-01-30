93b43efb0e2d022bcbce679d77c5e18c676e4016
j0jds <mrlucassilva7342@gmail.com>
2025-01-30 17:20:38 -0300

    Changed size and added references

--- RF/src/app/components/components.module.ts
+++ RF/src/app/components/components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from "@angular/core";
 import { PipesModule } from "../pipes/pipes.module";
 import { CommonModule } from "@angular/common";
 import { UsersListComponent } from './users-list/users-list.component';
+import { UserInfoItemComponent } from './user-info-item/user-info-item.component';
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 import { GeneralInformationsComponent } from './general-informations/general-informations.component';
 
 @NgModule({
     declarations: [    
       UsersListComponent, 
-      GeneralInformationsComponent
+      UserInfoItemComponent,
+      GeneralInformationsComponent, 
   ],
     imports: [
         AngularMaterialModule,
