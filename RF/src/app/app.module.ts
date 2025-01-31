b1c4cbfbafde4d6bab38139e06ae5393328531b2
j0jds <mrlucassilva7342@gmail.com>
2025-01-31 17:56:34 -0300

    Added references for currency pipe

--- RF/src/app/app.module.ts
+++ RF/src/app/app.module.ts
@@ -1,8 +1,11 @@
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt-BR');
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from "./components/components.module";
+import { registerLocaleData } from '@angular/common';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
