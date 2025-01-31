c47bfc711bac8f64080a877acda3518bc316f5ee
j0jds <mrlucassilva7342@gmail.com>
2025-01-31 17:56:59 -0300

    Reactivate access (wip)

--- RF/src/app/app.component.ts
+++ RF/src/app/app.component.ts
@@ -1,10 +1,10 @@
+import { take } from 'rxjs';
 import { UsersService } from './services/users.service';
-// import { StatesService } from './services/states.service';
-// import { CitiesService } from './services/cities.service';
-// import { CountriesService } from './services/countries.service';
+import { StatesService } from './services/states.service';
+import { CitiesService } from './services/cities.service';
+import { CountriesService } from './services/countries.service';
 import { Component, OnInit } from '@angular/core';
 import { UsersListResponse } from './types/user-list-response.interface';
-import { take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +14,27 @@ import { take } from 'rxjs';
 
 export class AppComponent implements OnInit {
     usersList: UsersListResponse = [];
-    currentTabIndex: number = 0;
+    currentTabIndex: number = 1;
 
   constructor(
-    // private readonly _countriesService: CountriesService,
-    // private readonly _statesService: StatesService,
-    // private readonly _citiesService: CitiesService,
+    private readonly _countriesService: CountriesService,
+    private readonly _statesService: StatesService,
+    private readonly _citiesService: CitiesService,
     private readonly _usersService: UsersService,
   ) {}
 
   ngOnInit(): void {
-    // this._countriesService.getCountries().subscribe((countriesResponse) => {
-    //   console.log('countriesResponse', countriesResponse);
-    // });
+    this._countriesService.getCountries().subscribe((countriesResponse) => {
+      console.log('countriesResponse', countriesResponse);
+    });
 
-    // this._statesService.getStates('Brazil').subscribe((statesResponse) => {
-    //   console.log('statesResponse', statesResponse);
-    // });
+    this._statesService.getStates('Brazil').subscribe((statesResponse) => {
+      console.log('statesResponse', statesResponse);
+    });
 
-    // this._citiesService.getCities('Brazil', 'Pernambuco').subscribe((citiesResponse) => {
-    //   console.log('citiesResponse', citiesResponse);
-    // });
+    this._citiesService.getCities('Brazil', 'Pernambuco').subscribe((citiesResponse) => {
+      console.log('citiesResponse', citiesResponse);
+    });
     
     this._usersService.getUsers().pipe(take(1)).subscribe((usersListResponse) => this.usersList = usersListResponse);
   }
